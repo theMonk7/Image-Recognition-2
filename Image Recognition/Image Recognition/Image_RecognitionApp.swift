da3055199828b6d6e5a08b40435a07beecf51e42
utkarshraj <utkarshraj@urbancompany.com>
2024-11-01 19:40:28 +0530

    Initial commit

---
+++ Image Recognition/Image Recognition/Image_RecognitionApp.swift
@@ -0,0 +1,17 @@
+//
+//  Image_RecognitionApp.swift
+//  Image Recognition
+//
+//  Created by Utkarsh Raj on 01/11/24.
+//
+
+import SwiftUI
+
+@main
+struct Image_RecognitionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
